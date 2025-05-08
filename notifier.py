79a799be5b94c85d3f02219cd4e94bcb8c34292c
InstrinnaI <romansidorenko941@gmail.com>
2025-05-08 21:32:11 +0300

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -1,19 +1,19 @@
-from telegram import Bot
+from telegram import Update
+from telegram.ext import Application, ContextTypes
 import logging
 
 class TelegramNotifier:
     def __init__(self, token, channel_id):
-        # передаём оба параметра напрямую
-        self.bot = Bot(token=token)
+        self.application = Application.builder().token(token).build()
         self.channel = channel_id
 
-    def send(self, video):
+    async def send(self, video):
         text = (
             f"🔔 Новое видео (дата публикации: {video['published_at']}):\n"
             f"{video['link']}"
         )
         try:
-            self.bot.send_message(self.channel, text)
+            await self.application.bot.send_message(self.channel, text)
             logging.info(f"Sent video {video['id']}")
         except Exception as e:
             logging.error(f"Failed to send message: {e}")
